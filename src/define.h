8109e21c47b3a14325a768679430b6dc171241c2
Luis Valencia <vluis@protonmail.com>
2025-11-21 22:28:21 -0600

    feat: static re-definitions

---
+++ src/define.h
@@ -0,0 +1,3 @@
+#define global static // global-scope variables | variables con alcance global
+#define persist static // local-scope variables | variables con alcance local
+#define local static // functions | funciones
--- src/main.c
+++ src/main.c
@@ -1,6 +1,25 @@
+#include "define.h"
 #include "types.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+global int64 a;
+
+local uint64 incrementCounter(void)
+{
+	persist uint64 c = 0;
+	c++;
+
+	return c;
+}
 
 int32 main(void)
 {
-	return 0; // TODO(vluis): Replace with EXIT_SUCCESS
+	for (int i = 0; i < 10; ++i) {
+		printf("%lu\n", a = incrementCounter());
+	}
+	printf("a = %lu\n", a);
+
+	return EXIT_SUCCESS; // Finalizar con éxito
 }
+
