8287d729f5979771c3e9635e89bdaeabb6388ef5
Luis Valencia <vluis@protonmail.com>
2025-11-24 21:00:40 -0600

    chore: added header and footer comments

--- src/defines.h
+++ src/defines.h
@@ -1,3 +1,7 @@
+/* defines.h: common definitions | definiciones comúnes */
+
 #define global static // global-scope variables | variables con alcance global
 #define persist static // local-scope variables | variables con alcance local
 #define local static // functions | funciones
+
+/* 24/11/2025 Luis Arturo Ramos Valencia - kanso engine */
