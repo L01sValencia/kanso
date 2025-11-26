1eaecd6a4ed507a15f797d5265fc8cc4cb12a5a9
Luis Valencia <vluis@protonmail.com>
2025-11-25 21:55:21 -0600

    feat: basic logging system

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/env bash
-clang -std=c23 -O0 -g src/main.c -o bin/main
+clang -std=c23 -O0 -g src/main.c -o bin/main -DKSO_DEBUG=1 # -DKSO_VDEBUG=1
--- src/main.c
+++ src/main.c
@@ -1,42 +1,11 @@
 #include "defines.h"
 #include "types.h"
 #include <stdlib.h>
-#include <stdio.h>
 
-/* logging */
-
-typedef enum { LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_FATAL } LogType;
-/* LogType descriptions | descripciones de LogType
- * LOG_TRACE: Verbose or highly frequent debugging messages | Mensajes con fines de debugging verbosos o muy frecuentes
- * LOG_DEBUG: Debugging messages | Mensajes de debugging
- * LOG_INFO: Information of errorless events | Información de eventos sin errores
- * LOG_WARN: Warning of a non-critical error | Advertencia de un error no crítico
- * LOG_ERROR: Critical but non-terminating error | Error crítico pero sin finalizar la ejecución
- * LOG_FATAL: Critical and unexpected terminating error | Error crítico inesperado que finaliza la ejecución
- */
-
-char* log_type_tags[] = { "TRACE", "DEBUG", "INFO", "WARNING", "ERROR", "FATAL" };
-
-void logMessage(LogType message_type, const char* message)
-{
-	FILE* output; // salida destino del mensaje
-	if (message_type <= LOG_INFO) {
-		output = stdout;
-	} else {
-		output = stderr;
-	}
-	fprintf(output, "%s: %s\n", log_type_tags[message_type], message);
-}
+#include "log.c"
 
 int32 main(void)
 {
-	logMessage(LOG_TRACE, "Mensaje Trace");
-	logMessage(LOG_DEBUG, "Mensaje Debug");
-	logMessage(LOG_INFO, "Mensaje Info");
-	logMessage(LOG_WARN, "Mensaje Warning");
-	logMessage(LOG_ERROR, "Mensaje Error");
-	logMessage(LOG_FATAL, "Mensaje Fatal");
-
 	return EXIT_SUCCESS; // Finalizar con éxito
 }
 
