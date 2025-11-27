34cc695084f80bcd5e08402a358393b2406c19f0
Luis Valencia <vluis@protonmail.com>
2025-11-26 22:35:23 -0600

    chore: improved renaming of static (macro definition)

--- src/defines.h
+++ src/defines.h
@@ -2,7 +2,7 @@
 
 #pragma once
 
-#define global static // global-scope variables | variables con alcance global
+#define global_variable static // global-scope variables | variables con alcance global
 #define persist static // local-scope variables | variables con alcance local
 #define internal static // confine functions to its translation unit | confinar funciones a su unidad de traducción
 
